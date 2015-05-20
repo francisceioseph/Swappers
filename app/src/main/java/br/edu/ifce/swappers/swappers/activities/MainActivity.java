d015be501db71ed7ca8c8e67a6238b6ed58608cd
Francisco José A. C. Souza <francisceioseph@gmail.com>
2015-05-20 14:18:44 -0300

    - Criação dos Ícones do Menu - Mudança de Biblioteca para uma mais similar ao do Navigation Drawer do Google

--- app/src/main/java/br/edu/ifce/swappers/swappers/activities/MainActivity.java
+++ app/src/main/java/br/edu/ifce/swappers/swappers/activities/MainActivity.java
@@ -1,9 +1,9 @@
 package br.edu.ifce.swappers.swappers.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import br.edu.ifce.swappers.swappers.R;
 import br.edu.ifce.swappers.swappers.fragments.principal.AboutFragment;
@@ -12,99 +12,71 @@ import br.edu.ifce.swappers.swappers.fragments.principal.PlacesFragment;
 import br.edu.ifce.swappers.swappers.fragments.principal.ProfileFragment;
 import br.edu.ifce.swappers.swappers.fragments.principal.SettingsFragment;
 import br.edu.ifce.swappers.swappers.fragments.principal.StatisticsFragment;
-import it.neokree.googlenavigationdrawer.GAccount;
-import it.neokree.googlenavigationdrawer.GAccountListener;
-import it.neokree.googlenavigationdrawer.GSection;
-import it.neokree.googlenavigationdrawer.GoogleNavigationDrawer;
+import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
+import it.neokree.materialnavigationdrawer.elements.MaterialAccount;
+import it.neokree.materialnavigationdrawer.elements.MaterialSection;
+import it.neokree.materialnavigationdrawer.elements.listeners.MaterialAccountListener;
 
-public class MainActivity extends GoogleNavigationDrawer implements GAccountListener {
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+public class MainActivity extends MaterialNavigationDrawer implements MaterialAccountListener{
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void init(Bundle savedInstance) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void init(Bundle bundle) {
-
-        GAccount userAccount = this.loadAccount();
+        MaterialAccount userAccount = this.loadAccount();
 
         this.addAccount(userAccount);
         this.setAccountListener(this);
 
         this.buildMainMenu();
-        this.buildBottomMenu();
     }
 
     private void buildMainMenu() {
 
-        //TODO load icons from resources
-        Drawable profileSectionIcon;
-        Drawable booksSectionIcon;
-        Drawable placesSectionIcon;
-        Drawable statisticsSectionIcon;
+        Drawable profileSectionIcon     = this.getResources().getDrawable(R.drawable.ic_person);
+        Drawable booksSectionIcon       = this.getResources().getDrawable(R.drawable.ic_book);
+        Drawable placesSectionIcon      = this.getResources().getDrawable(R.drawable.ic_room);
+        Drawable statisticsSectionIcon  = this.getResources().getDrawable(R.drawable.ic_dashboard);
+        Drawable aboutSectionIcon       = this.getResources().getDrawable(R.drawable.ic_help);
+        Drawable settingsSectionIcon    = this.getResources().getDrawable(R.drawable.ic_settings);
 
-        GSection profileSection     = this.newSection("Profile", new ProfileFragment());
-        GSection booksSection       = this.newSection("Books", new BooksFragment());
-        GSection placesSection      = this.newSection("Places", new PlacesFragment());
-        GSection statisticsSection  = this.newSection("Statistics", new StatisticsFragment());
+        MaterialSection profileSection     = this.newSection("Profile", profileSectionIcon, new ProfileFragment());
+        MaterialSection booksSection       = this.newSection("Books", booksSectionIcon, new BooksFragment());
+        MaterialSection placesSection      = this.newSection("Places", placesSectionIcon, new PlacesFragment());
+        MaterialSection statisticsSection  = this.newSection("Statistics", statisticsSectionIcon, new StatisticsFragment());
+        MaterialSection aboutSection       = this.newSection("About", aboutSectionIcon, new AboutFragment());
+        MaterialSection settingsSection    = this.newSection("Settings", settingsSectionIcon, new SettingsFragment());
 
         this.addSection(profileSection);
         this.addSection(booksSection);
         this.addSection(placesSection);
         this.addSection(statisticsSection);
+
         this.addDivisor();
-    }
-
-    private void buildBottomMenu() {
-        Drawable aboutSectionIcon;
-        Drawable settingsSectionIcon;
-
-        GSection aboutSection    = this.newSection("About", new AboutFragment());
-        GSection settingsSection = this.newSection("Settings", new SettingsFragment());
 
         this.addSection(aboutSection);
         this.addSection(settingsSection);
     }
 
-    @Override
-    public void onAccountOpening(GAccount gAccount) {
+    private MaterialAccount loadAccount(){
+        String username     = this.loadUserName();
+        String email        = this.loadUserEmail();
+        Bitmap userPhoto    = this.loadUserPhoto();
+        Bitmap coverPhoto   = this.loadCoverPhoto();
 
-    }
-
-    private GAccount loadAccount(){
-        String username         = this.loadUserName();
-        String email            = this.loadUserEmail();
-        Drawable profilePicture = this.loadUserPicture();
-        Drawable coverPicture   = this.loadCoverPicture();
-
-        GAccount userAccount = new GAccount(username, email, profilePicture, coverPicture);
+        MaterialAccount userAccount = new MaterialAccount(this.getResources(), username, email,userPhoto, coverPhoto);
         return userAccount;
     }
 
-    private Drawable loadCoverPicture() {
-        return this.getResources().getDrawable(R.drawable.background_splash);
+    private Bitmap loadCoverPhoto() {
+        BitmapDrawable coverPhotoDrawable = (BitmapDrawable) this.getResources().getDrawable(R.drawable.background_splash);
+        Bitmap coverPhotoBitmap = coverPhotoDrawable.getBitmap();
+        return coverPhotoBitmap;
     }
 
-    private Drawable loadUserPicture() {
-        return this.getResources().getDrawable(R.drawable.ic_launcher);
+    private Bitmap loadUserPhoto() {
+        BitmapDrawable userPhotoDrawer = (BitmapDrawable) this.getResources().getDrawable(R.drawable.ic_launcher);
+        Bitmap userPhotoBitmap = userPhotoDrawer.getBitmap();
+        return userPhotoBitmap;
     }
 
     private String loadUserEmail() {
@@ -115,4 +87,13 @@ public class MainActivity extends GoogleNavigationDrawer implements GAccountList
         return "Francis Thomas";
     }
 
+    @Override
+    public void onAccountOpening(MaterialAccount materialAccount) {
+
+    }
+
+    @Override
+    public void onChangeAccount(MaterialAccount materialAccount) {
+
+    }
 }
