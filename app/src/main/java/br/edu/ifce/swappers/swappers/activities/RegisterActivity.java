62d83811f908053a1b6a5f4817d850c3f89f7267
Francisco José A. C. Souza <francisceioseph@gmail.com>
2015-05-28 16:01:24 -0300

    - Criação de métodos para capturar fotos da galeria e da câmera. - Subtituição do botão de carregar imagem por um imageview circular - mudança do ícone de adicionar foto de usuário

--- app/src/main/java/br/edu/ifce/swappers/swappers/activities/RegisterActivity.java
+++ app/src/main/java/br/edu/ifce/swappers/swappers/activities/RegisterActivity.java
@@ -1,43 +1,57 @@
 package br.edu.ifce.swappers.swappers.activities;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import at.markushi.ui.CircleButton;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import br.edu.ifce.swappers.swappers.R;
 import br.edu.ifce.swappers.swappers.fragments.dialogs.UserPhotoDialogFragment;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.view.View.*;
 
 public class RegisterActivity extends AppCompatActivity implements UserPhotoDialogFragment.UserPhotoDialogListener{
 
-    Toolbar toolbar;
-    Button  registerButton;
-    CircleButton userPhotoCircleButton;
+    private Toolbar toolbar;
+    private Button  registerButton;
+    private CircleImageView userPhotoCircleImageView;
+
+    private static final short CAMERA_INTENT_CODE  = 1015;
+    private static final short GALLERY_INTENT_CODE = 1016;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        this.toolbar                = (Toolbar) findViewById(R.id.toolbar);
-        this.registerButton         = (Button)  findViewById(R.id.registerButton);
-        this.userPhotoCircleButton  = (CircleButton) findViewById(R.id.userPhotoCircleButton);
-
+        this.toolbar                  = (Toolbar) findViewById(R.id.toolbar);
+        this.registerButton           = (Button)  findViewById(R.id.registerButton);
+        this.userPhotoCircleImageView = (CircleImageView) findViewById(R.id.userPhotoCircleImageView);
 
         this.setUpActivityToolbar();
         this.registerButton.setOnClickListener(this.makeRegisterButtonClickListener());
-        this.userPhotoCircleButton.setOnClickListener(this.makeUserPhotoCircleButtonClickListener());
+        this.userPhotoCircleImageView.setOnClickListener(this.makeUserPhotoCircleButtonClickListener());
 
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,9 +75,21 @@ public class RegisterActivity extends AppCompatActivity implements UserPhotoDial
         return super.onOptionsItemSelected(item);
     }
 
-    /*
-     * Helper Methods for internal Activity use
-     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bitmap userPhotoBitmap;
+
+        if (requestCode == CAMERA_INTENT_CODE && resultCode == RESULT_OK){
+
+            userPhotoBitmap = this.retrieveImageFromCameraResult(data);
+            this.userPhotoCircleImageView.setImageBitmap(userPhotoBitmap);
+        }
+        else if (requestCode == GALLERY_INTENT_CODE && resultCode == RESULT_OK) {
+
+            userPhotoBitmap = this.retrieveImageFromGalleryResult(data);
+            this.userPhotoCircleImageView.setImageBitmap(userPhotoBitmap);
+        }
+    }
 
     private void setUpActivityToolbar(){
         if (this.toolbar != null){
@@ -71,8 +97,14 @@ public class RegisterActivity extends AppCompatActivity implements UserPhotoDial
         }
     }
 
-    private View.OnClickListener makeUserPhotoCircleButtonClickListener() {
-        View.OnClickListener clickListener = new View.OnClickListener() {
+    /*
+    *
+    * Listener builder methods
+    *
+    * */
+
+    private OnClickListener makeUserPhotoCircleButtonClickListener() {
+        OnClickListener clickListener = new OnClickListener() {
             @Override
             public void onClick(View v) {
                 UserPhotoDialogFragment dialogFragment = new UserPhotoDialogFragment();
@@ -83,13 +115,13 @@ public class RegisterActivity extends AppCompatActivity implements UserPhotoDial
         return clickListener;
     }
 
-    private View.OnClickListener makeRegisterButtonClickListener(){
+    private OnClickListener makeRegisterButtonClickListener(){
 
-        View.OnClickListener clickListener = new View.OnClickListener() {
+        OnClickListener clickListener = new OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (validateRegisterForm() == true){
+                if (isRegisterFormValid()){
                     RegisterActivity.this.startMainActivity();
                 }
                 else{
@@ -101,7 +133,13 @@ public class RegisterActivity extends AppCompatActivity implements UserPhotoDial
         return clickListener;
     }
 
-    private boolean validateRegisterForm(){
+    /*
+    *
+    * Validation methods for the form fields
+    *
+    * */
+
+    private boolean isRegisterFormValid(){
         //TODO Write a code here to validate the register form.
 
         return true;
@@ -118,22 +156,47 @@ public class RegisterActivity extends AppCompatActivity implements UserPhotoDial
         this.startActivity(mainActivityIntent);
     }
 
-    private void openPhotoGallery(){
-        //TODO Open a Gallery
-    }
 
-    private void openCamera(){
-        //TODO Open a Camera
-    }
+    /*
+    *
+    * Delegate methods from UserPhotoDialogFragment
+    *
+    * */
 
-//    Methods from UserPhotoDialogFragment
     @Override
     public void onGalleryClick(DialogFragment dialogFragment) {
-        //TODO Open Gallery Here
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY_INTENT_CODE);
+
     }
 
     @Override
     public void onCameraClick(DialogFragment dialogFragment) {
-        //TODO Open Camera Here
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(cameraIntent.resolveActivity( getPackageManager() ) != null) {
+            startActivityForResult(cameraIntent, CAMERA_INTENT_CODE);
+        }
+    }
+
+    private Bitmap retrieveImageFromCameraResult(Intent data){
+        Bundle extras = data.getExtras();
+        Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+        return imageBitmap;
+    }
+
+    private Bitmap retrieveImageFromGalleryResult(Intent data){
+        Uri selectedImageUri = data.getData();
+        String[] filePathColumn = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(selectedImageUri, filePathColumn, null, null, null);
+
+        cursor.moveToFirst();
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String picturePath = cursor.getString(columnIndex);
+        cursor.close();
+
+        Bitmap image = BitmapFactory.decodeFile(picturePath);
+        return image;
     }
 }
