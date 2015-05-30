762914486fee57d895c02fa63b1510b26a480279
Francisco José A. C. Souza <francisceioseph@gmail.com>
2015-05-30 17:46:07 -0300

    Na classe NearBooksFragment e seu respectivo XML:
    - Criação dos botões de previous e next near book.
    - Criação de métodos de inicialização para os principais componentes da view
    - Inicialização do listeners dos botões

--- app/src/main/java/br/edu/ifce/swappers/swappers/fragments/tabs/books/NearBooksFragment.java
+++ app/src/main/java/br/edu/ifce/swappers/swappers/fragments/tabs/books/NearBooksFragment.java
@@ -6,26 +6,84 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import br.edu.ifce.swappers.swappers.R;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NearBooksFragment extends Fragment {
 
+    ImageButton nextNearBookImageButton;
+    ImageButton previousNearBookImageButton;
+    Button seePlacesOnMapsButton;
+
+    CircleImageView coverNearBookCircleImageView;
+
+    TextView titleNearBookTextView;
+    TextView authorsNearBookTextView;
 
     public NearBooksFragment() {
-        // Required empty public constructor
+
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_near_books, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_near_books, container, false);
+
+        this.initViewComponents(rootView);
+        this.initViewListeners();
+
+        return rootView;
     }
 
+    private void initViewComponents(View rootView) {
+        this.nextNearBookImageButton      = (ImageButton) rootView.findViewById(R.id.next_book_image_button);
+        this.previousNearBookImageButton  = (ImageButton) rootView.findViewById(R.id.previous_book_image_button);
+        this.seePlacesOnMapsButton        = (Button) rootView.findViewById(R.id.see_places_of_near_book);
 
+        this.coverNearBookCircleImageView = (CircleImageView) rootView.findViewById(R.id.cover_near_book);
+
+        this.titleNearBookTextView        = (TextView) rootView.findViewById(R.id.title_near_book);
+        this.authorsNearBookTextView      = (TextView) rootView.findViewById(R.id.authors_near_book);
+    }
+
+    private void initViewListeners() {
+        this.nextNearBookImageButton.setOnClickListener(this.nextNearBookClickListener());
+        this.previousNearBookImageButton.setOnClickListener(this.previousNearBookClickListener());
+        this.seePlacesOnMapsButton.setOnClickListener(this.seePlacesOnMapsClickListener());
+    }
+
+    private View.OnClickListener seePlacesOnMapsClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Opening Map... #SQN", Toast.LENGTH_SHORT).show();;
+            }
+        };
+    }
+
+    private View.OnClickListener previousNearBookClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Loading previous book... #SQN", Toast.LENGTH_SHORT).show();;
+            }
+        };
+    }
+
+    private View.OnClickListener nextNearBookClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Opening next book... #SQN", Toast.LENGTH_SHORT).show();;
+            }
+        };
+    }
 }
