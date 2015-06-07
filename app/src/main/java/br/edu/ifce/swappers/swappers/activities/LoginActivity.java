8a9ea14ef95c60eaed798657bbca770d7936a771
Francisco José A. C. Souza <francisceioseph@gmail.com>
2015-06-07 20:46:55 -0300

    - Alguns reparos de pequenos bugs para a versão 4.4.4 do Android.

--- app/src/main/java/br/edu/ifce/swappers/swappers/activities/LoginActivity.java
+++ app/src/main/java/br/edu/ifce/swappers/swappers/activities/LoginActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import br.edu.ifce.swappers.swappers.R;
 
@@ -18,20 +19,36 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        Button signUpButton = (Button) findViewById(R.id.sign_up_button);
 
         if (toolbar != null){
             setSupportActionBar(toolbar);
         }
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.startMainActivity();
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.startRegisterActivity();
+            }
+        });
     }
 
-    public void startMainActivity(View view){
+    public void startMainActivity(){
         Intent mainActivityIntent = new Intent(this, MainActivity.class);
         mainActivityIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
 
         this.startActivity(mainActivityIntent);
     }
 
-    public void startRegisterActivity(View view){
+    public void startRegisterActivity(){
         Intent registerActivityIntent = new Intent(this, RegisterActivity.class);
         //registerActivityIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
 
