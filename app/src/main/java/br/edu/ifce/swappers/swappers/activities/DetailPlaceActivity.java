20a2f5c3e7b61facbca8bd2ee9c8d7f87a2140b5
Francisco José A. C. Souza <francisceioseph@gmail.com>
2015-06-07 16:22:21 -0300

    - Eliminação de menus da toolbar que são desnecessários.

--- app/src/main/java/br/edu/ifce/swappers/swappers/activities/DetailPlaceActivity.java
+++ app/src/main/java/br/edu/ifce/swappers/swappers/activities/DetailPlaceActivity.java
@@ -14,26 +14,4 @@ public class DetailPlaceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_place);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail_place, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
