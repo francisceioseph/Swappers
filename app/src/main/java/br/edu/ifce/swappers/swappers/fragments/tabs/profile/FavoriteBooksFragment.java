462da59502516f3b1e8639f91b9978a288096772
francisceioseph <francisceioseph@gmail.com>
2015-06-21 11:46:38 -0300

    - Implementação do ListView na tela de profile, aba de livros favoritos.

--- app/src/main/java/br/edu/ifce/swappers/swappers/fragments/tabs/profile/FavoriteBooksFragment.java
+++ app/src/main/java/br/edu/ifce/swappers/swappers/fragments/tabs/profile/FavoriteBooksFragment.java
@@ -3,29 +3,52 @@ package br.edu.ifce.swappers.swappers.fragments.tabs.profile;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import br.edu.ifce.swappers.swappers.MockSingleton;
 import br.edu.ifce.swappers.swappers.R;
+import br.edu.ifce.swappers.swappers.adapters.BookRecyclerViewAdapter;
+import br.edu.ifce.swappers.swappers.model.Book;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FavoriteBooksFragment extends Fragment {
-
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    ArrayList<Book> dataSource;
 
     public FavoriteBooksFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_books, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dataSource = MockSingleton.INSTANCE.createMockedBookDataSource();
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_favorite_books, container, false);
 
+        BookRecyclerViewAdapter adapter = new BookRecyclerViewAdapter(dataSource);
+
+        this.layoutManager = new LinearLayoutManager(getActivity());
+        this.recyclerView = (RecyclerView) rootView.findViewById(R.id.favorite_books_list);
+
+        this.recyclerView.setHasFixedSize(false);
+        this.recyclerView.setLayoutManager(layoutManager);
+        this.recyclerView.setAdapter(adapter);
+        this.recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        return rootView;
+
+    }
 }
