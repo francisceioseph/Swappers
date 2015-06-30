bad36805bc87de8bce3cdca38647a5063b600bb8
Joamila Brito <joamila.brito@gmail.com>
2015-06-29 23:12:21 -0300

    - Ajustes na obtenção das coordenadas pelo GPS.

--- app/src/main/java/br/edu/ifce/swappers/swappers/fragments/principal/PlacesFragment.java
+++ app/src/main/java/br/edu/ifce/swappers/swappers/fragments/principal/PlacesFragment.java
@@ -23,10 +23,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import br.edu.ifce.swappers.swappers.R;
 import br.edu.ifce.swappers.swappers.util.SwappersToast;
 
-public class PlacesFragment extends Fragment {
+
+public class PlacesFragment extends Fragment{
 
     MapView mapView;
-    GoogleMap mapPlace;
+    static GoogleMap mapPlace;
     LocationManager locationManager;
 
     private final LatLng SHOPPING_BENFICA = new LatLng(-3.739126, -38.5402);
@@ -42,29 +43,26 @@ public class PlacesFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Listener listener = new Listener();
+        long timeUpdate = 5000;
+        float distance = 0;
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_places, container, false);
 
         locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
-        Listener listener = new Listener();
-        long timeUpdate = 0;
-        float distance = 0;
-
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, timeUpdate, distance, listener);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, timeUpdate, distance, listener);
 
         Location locationUser = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
         if(locationUser == null){
             Toast toast = SwappersToast.makeText(getActivity(), "Connect the GPS!", Toast.LENGTH_LONG);
             toast.setGravity(Gravity.CENTER, 0, 0);
             toast.show();
 
-            /*OBS: por enquanto, para que a aplicação não quebre, se o gps estiver desligado, o mapa abrirá em um
-            local especificado previamente.*/
+            //se o gps estiver desligado, o mapa abrirá em um local previamente definido.
             myPosition = IFCE_FORTALEZA;
         }
         else myPosition = listener.getMyPosition(locationUser);
@@ -74,11 +72,11 @@ public class PlacesFragment extends Fragment {
 
         mapPlace = mapView.getMap();
         mapPlace.getUiSettings().setMyLocationButtonEnabled(true);
+        mapPlace.getUiSettings().setMapToolbarEnabled(true);
         mapPlace.setMyLocationEnabled(true);
         mapPlace.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
         MapsInitializer.initialize(this.getActivity());
-
         setUpMap();
 
         return view;
@@ -107,40 +105,54 @@ public class PlacesFragment extends Fragment {
         mapPlace.moveCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 14));
         mapPlace.animateCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 14));
 
-        mapPlace.addMarker(new MarkerOptions().position(myPosition).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_person)));
-
         mapPlace.addMarker(new MarkerOptions().position(SHOPPING_BENFICA)
-                                        .title("Shopping Benfica")
-                                        .visible(true)
-                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+                .title("Shopping Benfica")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
         mapPlace.addMarker(new MarkerOptions().position(NORTH_SHOPPING)
                                         .title("North Shopping")
-                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
         mapPlace.addMarker(new MarkerOptions().position(SHOPPING_IGUATEMI)
                 .title("Shopping Iguatemi")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
-
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
 
     }
+
 }
 
     class Listener implements LocationListener {
+        float latitudeUser;
+        float longitudeUser;
+        LatLng userPosition;
+        private int changeZoom = 0;
 
         @Override
-        public void onLocationChanged(Location location){}
+        public void onLocationChanged(Location location){
+            latitudeUser = (float)location.getLatitude();
+            longitudeUser = (float)location.getLongitude();
+            userPosition = new LatLng(latitudeUser, longitudeUser);
+
+            if (changeZoom < 1) {
+                PlacesFragment.mapPlace.addMarker(new MarkerOptions().position(userPosition).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_portrait)));
+                PlacesFragment.mapPlace.moveCamera(CameraUpdateFactory.newLatLngZoom(userPosition, 14));
+                PlacesFragment.mapPlace.animateCamera(CameraUpdateFactory.zoomTo(14), 500, null);
+                changeZoom++;
+            }
+        }
 
         public void onStatusChanged(String provider, int status, Bundle extras){}
 
         @Override
-        public void onProviderEnabled(String provider){}
+        public void onProviderEnabled(String provider){
+
+        }
 
         @Override
         public void onProviderDisabled(String provider){}
 
         public LatLng getMyPosition(Location location){
-            float lat = (float) location.getLatitude();
-            float longi = (float) location.getLongitude();
-            LatLng myPosition = new LatLng(lat, longi);
+            float myPositionLatitude = (float) location.getLatitude();
+            float myPositionLongitude = (float) location.getLongitude();
+            LatLng myPosition = new LatLng(myPositionLatitude, myPositionLongitude);
 
             return myPosition;
         }
