8201e1c6d0c3f1a91cf29636ce287d07884ab898
Alessandra Lino <ale.lino.00@gmail.com>
2015-08-23 21:01:30 -0300

    Tentativa (falha) de inserir o efeito de tabhost do whatsapp android. Trabalhar mais nessa branch

--- app/src/main/java/br/edu/ifce/swappers/swappers/activities/DetailBookActivity.java
+++ app/src/main/java/br/edu/ifce/swappers/swappers/activities/DetailBookActivity.java
@@ -1,171 +1,152 @@
 package br.edu.ifce.swappers.swappers.activities;
 
+
+
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.os.Parcelable;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v4.app.FragmentTabHost;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TabHost;
-import android.widget.TabWidget;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.getbase.floatingactionbutton.FloatingActionButton;
+
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.edu.ifce.swappers.swappers.R;
 import br.edu.ifce.swappers.swappers.fragments.tabs.detail_book.ReadersCommentsFragment;
 import br.edu.ifce.swappers.swappers.fragments.tabs.detail_book.SynopsisFragment;
 import br.edu.ifce.swappers.swappers.model.Book;
 import br.edu.ifce.swappers.swappers.util.AndroidUtils;
-import br.edu.ifce.swappers.swappers.util.SwappersToast;
 import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class DetailBookActivity extends AppCompatActivity{
 
-    private FragmentTabHost bookDetailTabHost;
     private boolean flag = true;
     TextView nameBook;
     TextView authourBook;
     TextView editorBook;
     CircleImageView photoBook;
     Toolbar toolbar;
-    CollapsingToolbarLayout collapsingToolbarLayout;
     Book book;
+    private DrawerLayout mDrawerLayout;
 
-    public DetailBookActivity(){
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_detail_book);
 
-    }
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle("Detail Book");
+            setSupportActionBar(toolbar);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_book);
+            final ActionBar ab = getSupportActionBar();
 
-        Intent currentIntent = getIntent();
+            ab.setDisplayHomeAsUpEnabled(true);
 
-        nameBook = (TextView) findViewById(R.id.title_detail_book);
-        authourBook = (TextView) findViewById(R.id.author_detail_book);
-        editorBook = (TextView) findViewById(R.id.editor_detail_book);
-        photoBook = (CircleImageView) findViewById(R.id.photoBook);
+            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+            ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+            if (viewPager != null) {
+                setupViewPager(viewPager);
+            }
 
 
-        book = (Book) currentIntent.getSerializableExtra(AndroidUtils.SELECTED_BOOK_ID);
+            Intent currentIntent = getIntent();
+            book = (Book) currentIntent.getSerializableExtra(AndroidUtils.SELECTED_BOOK_ID);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        toolbar.setTitle("Detail Book");
+            nameBook = (TextView) findViewById(R.id.title_detail_book);
+            authourBook = (TextView) findViewById(R.id.author_detail_book);
+            editorBook = (TextView) findViewById(R.id.editor_detail_book);
+            photoBook = (CircleImageView) findViewById(R.id.photoBook);
 
-        if(book.getTitle().length() > 45){
-            StringBuilder titleBook = new StringBuilder(book.getTitle());
-            titleBook.setCharAt(42, '.');
-            titleBook.setCharAt(43, '.');
-            titleBook.setCharAt(44, '.');
-            nameBook.setText(titleBook);
-        }else{
-            nameBook.setText(book.getTitle());
-        }
+            onCreateLabels();
+            initFloatingButtons();
 
-        if(book.getAuthor().length() > 20){
-            StringBuilder authorBook = new StringBuilder(book.getAuthor());
-            authorBook.setCharAt(18, '.');
-            authorBook.setCharAt(19, '.');
-            authorBook.setCharAt(20, '.');
-            authourBook.setText(authorBook);
-        }else{
-            authourBook.setText(book.getAuthor());
-        }
-
-        if(book.getPublisher().length() > 25){
-            StringBuilder editorBooks = new StringBuilder(book.getPublisher());
-            editorBooks.setCharAt(22, '.');
-            editorBooks.setCharAt(23, '.');
-            editorBooks.setCharAt(24, '.');
-            editorBook.setText(editorBooks);
-        }else {
-            editorBook.setText(book.getPublisher());
+            TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+            tabLayout.setupWithViewPager(viewPager);
         }
 
 
-        if(!book.getPhoto().isEmpty()) {
-            Picasso.with(getApplicationContext()).load(book.getPhoto()).into(photoBook);
-        }else{
-            Picasso.with(getApplicationContext()).load(R.drawable.blue_book).into(photoBook);
+        public void onCreateLabels(){
+            if(book.getTitle().length() > 45){
+                StringBuilder titleBook = new StringBuilder(book.getTitle());
+                titleBook.setCharAt(42, '.');
+                titleBook.setCharAt(43, '.');
+                titleBook.setCharAt(44, '.');
+                nameBook.setText(titleBook);
+            }else{
+                nameBook.setText(book.getTitle());
+            }
+
+            if(book.getAuthor().length() > 20){
+                StringBuilder authorBook = new StringBuilder(book.getAuthor());
+                authorBook.setCharAt(18, '.');
+                authorBook.setCharAt(19, '.');
+                authorBook.setCharAt(20, '.');
+                authourBook.setText(authorBook);
+            }else{
+                authourBook.setText(book.getAuthor());
+            }
+
+            if(book.getPublisher().length() > 25){
+                StringBuilder editorBooks = new StringBuilder(book.getPublisher());
+                editorBooks.setCharAt(22, '.');
+                editorBooks.setCharAt(23, '.');
+                editorBooks.setCharAt(24, '.');
+                editorBook.setText(editorBooks);
+            }else {
+                editorBook.setText(book.getPublisher());
+            }
+
+            if(!book.getPhoto().isEmpty()) {
+                Picasso.with(getApplicationContext()).load(book.getPhoto()).into(photoBook);
+            }else{
+                Picasso.with(getApplicationContext()).load(R.drawable.blue_book).into(photoBook);
+            }
         }
 
-        this.initFloatingButtons();
-        this.initToolbar();
-        this.initTabHost();
-
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        this.bookDetailTabHost = null;
-    }
-
-    private void initToolbar() {
-        if (toolbar != null){
-            this.setSupportActionBar(toolbar);
-            this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            this.getSupportActionBar().setHomeButtonEnabled(true);
-
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            return true;
         }
-    }
 
-    private void initTabHost() {
-
-        this.bookDetailTabHost = (FragmentTabHost) findViewById(R.id.books_tabHost);
-        this.bookDetailTabHost.setup(this, this.getSupportFragmentManager(), android.R.id.tabcontent);
-
-
-        TabHost.TabSpec synopsisTab     = this.bookDetailTabHost.newTabSpec("synopsisTab");
-        TabHost.TabSpec readersCommentsTab   = this.bookDetailTabHost.newTabSpec("readersCommentsTab");
-
-        synopsisTab.setIndicator("SYNOPSES");
-        readersCommentsTab.setIndicator("READERS COMMENTS");
-
-        this.bookDetailTabHost.addTab(synopsisTab, SynopsisFragment.class, null);
-        this.bookDetailTabHost.addTab(readersCommentsTab, ReadersCommentsFragment.class, null);
-
-        this.bookDetailTabHost.setCurrentTab(1);
-
-        this.stylizeTabsTextView(bookDetailTabHost);
-    }
-
-    private void stylizeTabsTextView(FragmentTabHost tabHost){
-        ColorStateList tabTextColors;
-        TabWidget tabWidget;
-        TextView tabTextView;
-        View tabView;
-
-        int tabAmount;
-
-        tabWidget     = tabHost.getTabWidget();
-        tabTextColors = this.getResources().getColorStateList(R.color.tab_text_selector);
-        tabAmount     = tabWidget.getTabCount();
-
-        for (int i = 0; i < tabAmount; i++){
-            tabView = tabWidget.getChildTabViewAt(i);
-            tabView.setBackgroundResource(R.drawable.tab_indicator);
-
-            tabTextView = (TextView) tabView.findViewById(android.R.id.title);
-            tabTextView.setTextColor(tabTextColors);
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            return super.onOptionsItemSelected(item);
         }
-    }
+
+        private void setupViewPager(ViewPager viewPager) {
+            Adapter adapter = new Adapter(getSupportFragmentManager());
+            adapter.addFragment(new SynopsisFragment(), "SYNOPSIS");
+            adapter.addFragment(new ReadersCommentsFragment(), "READERS COMMENTS");
+            viewPager.setAdapter(adapter);
+        }
+
+
 
     private void initFloatingButtons(){
 
-        FloatingActionButton adoptButton    = (FloatingActionButton) findViewById(R.id.floating_action_adop);
+      /*  FloatingActionButton adoptButton    = (FloatingActionButton) findViewById(R.id.floating_action_adop);
         FloatingActionButton donateButton   = (FloatingActionButton) findViewById(R.id.floating_action_donate);
         FloatingActionButton favoriteButton = (FloatingActionButton) findViewById(R.id.floating_action_favorite);
         FloatingActionButton commentButton  = (FloatingActionButton) findViewById(R.id.floating_action_comment);
@@ -173,8 +154,11 @@ public class DetailBookActivity extends AppCompatActivity{
         adoptButton.setOnClickListener(this.makeAdoptListener());
         donateButton.setOnClickListener(this.makeDonateListener());
         favoriteButton.setOnClickListener(this.makeFavoriteListener());
-        commentButton.setOnClickListener(this.makeCommentListener());
+        commentButton.setOnClickListener(this.makeCommentListener());*/
     }
+
+
+
 
     private View.OnClickListener makeCommentListener() {
 
@@ -194,7 +178,7 @@ public class DetailBookActivity extends AppCompatActivity{
                 ImageView imgView =(ImageView) findViewById(R.id.is_book_favorite);
 
                 if (flag){
-                    Drawable  drawable  = getResources().getDrawable(R.drawable.ic_is_book_favorite);
+                    Drawable drawable  = getResources().getDrawable(R.drawable.ic_is_book_favorite);
                     imgView.setImageDrawable(drawable);
                     flag = false;
                 }else {
@@ -230,4 +214,33 @@ public class DetailBookActivity extends AppCompatActivity{
             }
         };
     }
+        public class Adapter extends FragmentPagerAdapter {
+            private final List<Fragment> mFragments = new ArrayList<>();
+            private final List<String> mFragmentTitles = new ArrayList<>();
+
+            public Adapter(FragmentManager fm) {
+                super(fm);
+            }
+
+            public void addFragment(Fragment fragment, String title) {
+                mFragments.add(fragment);
+                mFragmentTitles.add(title);
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragments.size();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mFragmentTitles.get(position);
+            }
+        }
+
 }
