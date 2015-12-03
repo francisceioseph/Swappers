52bf432fbbeaff60699cdd52734d658ed05f86d6
Joamila Brito <joamila.brito@gmail.com>
2015-12-03 11:05:31 -0300

    - Livros: Geração aleatória de recomendações.

--- app/src/main/java/br/edu/ifce/swappers/swappers/fragments/tabs/books/NearBooksFragment.java
+++ app/src/main/java/br/edu/ifce/swappers/swappers/fragments/tabs/books/NearBooksFragment.java
@@ -44,7 +44,8 @@ public class NearBooksFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {        View rootView = inflater.inflate(R.layout.fragment_near_books, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_near_books, container, false);
 
         this.initViewComponents(rootView);
         this.initViewListeners();
--- app/src/main/java/br/edu/ifce/swappers/swappers/fragments/tabs/books/RecommendationsFragment.java
+++ app/src/main/java/br/edu/ifce/swappers/swappers/fragments/tabs/books/RecommendationsFragment.java
@@ -1,8 +1,10 @@
 package br.edu.ifce.swappers.swappers.fragments.tabs.books;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +13,19 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import br.edu.ifce.swappers.swappers.MockSingleton;
 import br.edu.ifce.swappers.swappers.R;
+import br.edu.ifce.swappers.swappers.activities.DetailPlaceActivity;
 import br.edu.ifce.swappers.swappers.miscellaneous.SwappersToast;
+import br.edu.ifce.swappers.swappers.model.Book;
+import br.edu.ifce.swappers.swappers.model.BookWithPlace;
+import br.edu.ifce.swappers.swappers.model.Place;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RecommendationsFragment extends Fragment {
@@ -26,6 +39,10 @@ public class RecommendationsFragment extends Fragment {
     TextView titleRecommendationTextView;
     TextView authorsRecommendationTextView;
 
+    List<Place> nearPlaces = MockSingleton.INSTANCE.places;
+    List<BookWithPlace> recommendationBooks = new ArrayList<>();
+    int indexBookRec = 0;
+
     public RecommendationsFragment() {
 
     }
@@ -38,8 +55,37 @@ public class RecommendationsFragment extends Fragment {
         this.initViewComponents(rootView);
         this.initViewListeners();
 
-        titleRecommendationTextView.setText("O Segundo Sexo");
-        authorsRecommendationTextView.setText("Simone de Beauvoir");
+        Log.i("INDEX", String.valueOf(indexBookRec));
+
+        if (nearPlaces!=null) {
+            for (int i = 0; i < nearPlaces.size(); i++) {
+                if (nearPlaces.get(i).getBooks().size()>0) {
+                    for (int j = 0; j<nearPlaces.get(i).getBooks().size(); j++){
+                        recommendationBooks.add(new BookWithPlace(nearPlaces.get(i).getBooks().get(j), nearPlaces.get(i).getId()));
+                    }
+                }
+            }
+
+            Collections.shuffle(recommendationBooks);
+
+            if(!recommendationBooks.isEmpty()) {
+                Picasso.with(getActivity()).load(recommendationBooks.get(0).getBook().getPhoto()).into(coverRecommendationCircleImageView);
+                titleRecommendationTextView.setText(recommendationBooks.get(0).getBook().getTitle());
+                authorsRecommendationTextView.setText(recommendationBooks.get(0).getBook().getAuthor());
+            }else{
+                Picasso.with(getActivity()).load(R.drawable.blue_book).into(coverRecommendationCircleImageView);
+                titleRecommendationTextView.setText("Ainda não há recomendações.");
+                authorsRecommendationTextView.setText("");
+            }
+        }else {
+            Picasso.with(getActivity()).load(R.drawable.blue_book).into(coverRecommendationCircleImageView);
+            titleRecommendationTextView.setText("Ainda não há recomendações.");
+            authorsRecommendationTextView.setText("");
+        }
+
+
+        //titleRecommendationTextView.setText("O Segundo Sexo");
+        //authorsRecommendationTextView.setText("Simone de Beauvoir");
 
         return rootView;
     }
@@ -65,7 +111,32 @@ public class RecommendationsFragment extends Fragment {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SwappersToast.makeText(getActivity(), getString(R.string.service_not_implemented), Toast.LENGTH_SHORT).show();;
+                //SwappersToast.makeText(getActivity(), getString(R.string.service_not_implemented), Toast.LENGTH_SHORT).show();;
+
+                if(!recommendationBooks.isEmpty()){
+                    for (int j=0; j<nearPlaces.size(); j++){
+                        if(recommendationBooks.get(indexBookRec).getIdPlace() == nearPlaces.get(j).getId()){
+                            Place place =  new Place();
+                            place.setId(nearPlaces.get(j).getId());
+                            place.setName(nearPlaces.get(j).getName());
+                            place.setStreet(nearPlaces.get(j).getStreet());
+                            place.setNumber(nearPlaces.get(j).getNumber());
+                            place.setDistrict(nearPlaces.get(j).getDistrict());
+                            place.setCity(nearPlaces.get(j).getCity());
+                            place.setStates(nearPlaces.get(j).getStates());
+                            place.setCep(nearPlaces.get(j).getCep());
+                            place.setHour_func(nearPlaces.get(j).getHour_func());
+                            place.setPhoto2(nearPlaces.get(j).getPhoto2());
+                            place.setBooks(nearPlaces.get(j).getBooks());
+
+                            Intent detailPlaceActivityIntent = new Intent(getActivity(), DetailPlaceActivity.class);
+                            detailPlaceActivityIntent.putExtra("SELECTED_BOOK_PLACE", place);
+                            startActivity(detailPlaceActivityIntent);
+                        }
+                    }
+                }
+
+
             }
         };
     }
@@ -74,7 +145,28 @@ public class RecommendationsFragment extends Fragment {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SwappersToast.makeText(getActivity(), getString(R.string.service_not_implemented), Toast.LENGTH_SHORT).show();;
+                //SwappersToast.makeText(getActivity(), getString(R.string.service_not_implemented), Toast.LENGTH_SHORT).show();;
+                Log.i("INDEX", String.valueOf(indexBookRec));
+                if(!recommendationBooks.isEmpty()){
+                    if(recommendationBooks.size()<=5){
+                        indexBookRec--;
+                        if (indexBookRec<0) indexBookRec = 0;
+                    }
+                    else if (recommendationBooks.size()>5){
+                        indexBookRec--;
+                        if (indexBookRec<0) indexBookRec = 4;// (recommendationBooks.size()/4)-1;
+                    }
+
+                    titleRecommendationTextView.setText(recommendationBooks.get(indexBookRec).getBook().getTitle());
+                    authorsRecommendationTextView.setText(recommendationBooks.get(indexBookRec).getBook().getAuthor());
+
+                    if(!recommendationBooks.get(indexBookRec).getBook().getPhoto().isEmpty()) {
+                        Picasso.with(getActivity()).load(recommendationBooks.get(indexBookRec).getBook().getPhoto()).
+                                into(coverRecommendationCircleImageView);
+                    }else{
+                        Picasso.with(getActivity()).load(R.drawable.blue_book).into(coverRecommendationCircleImageView);
+                    }
+                }
             }
         };
     }
@@ -83,7 +175,29 @@ public class RecommendationsFragment extends Fragment {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SwappersToast.makeText(getActivity(), getString(R.string.service_not_implemented), Toast.LENGTH_SHORT).show();;
+                //SwappersToast.makeText(getActivity(), getString(R.string.service_not_implemented), Toast.LENGTH_SHORT).show();;
+
+                if(!recommendationBooks.isEmpty()){
+                    if (recommendationBooks.size()<=5){
+                        indexBookRec++;
+                        if(indexBookRec>0) indexBookRec = 0;
+                    }
+                    else if (recommendationBooks.size()>5){
+                        indexBookRec++;
+                        if(indexBookRec>=5) indexBookRec = 0;
+                    }
+
+                    titleRecommendationTextView.setText(recommendationBooks.get(indexBookRec).getBook().getTitle());
+                    authorsRecommendationTextView.setText(recommendationBooks.get(indexBookRec).getBook().getAuthor());
+
+                    if(!recommendationBooks.get(indexBookRec).getBook().getPhoto().isEmpty()) {
+                        Picasso.with(getActivity()).load(recommendationBooks.get(indexBookRec).getBook().getPhoto()).
+                                into(coverRecommendationCircleImageView);
+                    }else{
+                        Picasso.with(getActivity()).load(R.drawable.blue_book).into(coverRecommendationCircleImageView);
+                    }
+                }
+
             }
         };
     }
